fefa059580b07b273a3b9ff44bfdcaadef6b2b54
zhilogs <jaehui327@naver.com>
2022-02-24 07:03:53 +0900

    Time: 109 ms (46.38%), Space: 14.8 MB (37.68%) - LeetHub

--- 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.swift
+++ 744-find-smallest-letter-greater-than-target/744-find-smallest-letter-greater-than-target.swift
@@ -1,10 +1,25 @@
 class Solution {
     func nextGreatestLetter(_ letters: [Character], _ target: Character) -> Character {
+        // Linear Scan
+        /*
         for l in letters {
             if l > target {
                 return l
             }
         }
         return letters[0]
+        */
+        // Binary Search
+        var left = 0
+        var right = letters.count
+        while left < right {
+            let middle = left + (right - left) / 2
+            if letters[middle] <= target {
+                left = middle + 1
+            } else {
+                right = middle
+            }
+        }
+        return letters[left % letters.count]
     }
 }

c36a9087134fb9173cedd0960d687f11c5a0a361
zhilogs <jaehui327@naver.com>
2022-02-23 18:02:27 +0900

    Time: 4 ms (92.31%), Space: 14.4 MB (10.26%) - LeetHub

--- 1732-find-the-highest-altitude/1732-find-the-highest-altitude.swift
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.swift
@@ -1,11 +1,13 @@
 class Solution {
     func largestAltitude(_ gain: [Int]) -> Int {
-        var altitudes = [0]
+        var max = 0
         var altitude = 0
         for g in gain {
             altitude += g
-            altitudes.append(altitude)
+            if max < altitude {
+                max = altitude
+            }
         }
-        return altitudes.max()!
+        return max
     }
 }

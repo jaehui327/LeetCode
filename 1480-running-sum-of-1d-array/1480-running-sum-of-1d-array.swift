8f26a222e5b6c301fbfe4bb80bbebe64c5fe8916
zhilogs <jaehui327@naver.com>
2022-02-28 20:38:02 +0900

    Time: 25 ms (33.54%), Space: 14.4 MB (11.80%) - LeetHub

--- 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.swift
+++ 1480-running-sum-of-1d-array/1480-running-sum-of-1d-array.swift
@@ -1,8 +1,10 @@
 class Solution {
     func runningSum(_ nums: [Int]) -> [Int] {
-        var result = [Int](repeating: nums[0], count: nums.count)
-        for i in 1 ... nums.count - 1 {
-            result[i] = result[i - 1] + nums[i]
+        var result: [Int] = []
+        var sum: Int = 0
+        for n in nums {
+            sum += n
+            result.append(sum)
         }
         return result
     }

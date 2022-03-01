a84519d949a8b034328045639faf693abfbd845b
zhilogs <jaehui327@naver.com>
2022-03-01 17:32:20 +0900

    Time: 24 ms (93.83%), Space: 14.5 MB (12.23%) - LeetHub

---
+++ 35-search-insert-position/35-search-insert-position.swift
@@ -0,0 +1,10 @@
+class Solution {
+    func searchInsert(_ nums: [Int], _ target: Int) -> Int {
+        for (i, n) in nums.enumerated() {
+            if n >= target {
+                return i
+            }
+        }
+        return nums.count
+    }
+}

7dd32c661ae1b3d471f0558d8a7b04775531e079
zhilogs <jaehui327@naver.com>
2022-03-08 06:48:34 +0900

    Time: 17 ms (61.54%), Space: 14.2 MB (92.31%) - LeetHub

---
+++ 1961-check-if-string-is-a-prefix-of-array/1961-check-if-string-is-a-prefix-of-array.swift
@@ -0,0 +1,12 @@
+class Solution {
+    func isPrefixString(_ s: String, _ words: [String]) -> Bool {
+        var prefix = ""
+        for word in words {
+            prefix += word
+            if s == prefix {
+                return true
+            }
+        }
+        return false
+    }
+}

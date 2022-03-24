0b30f69df4510cc42db9f6341ba3310bc8b474ed
zhilogs <jaehui327@naver.com>
2022-03-25 07:16:06 +0900

    Time: 4 ms (55.56%), Space: 14.3 MB (66.67%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.swift
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.swift
@@ -2,13 +2,13 @@ class Solution {
     func subsetXORSum(_ nums: [Int]) -> Int {
         var result = 0
         
-        func calculate(_ xor: Int, _ start: Int) {
-            guard start < nums.count else { return }
-            let next = xor ^ nums[start]
+        func calculate(_ xor: Int, _ index: Int) {
+            guard index < nums.count else { return }
+            let next = xor ^ nums[index]
             result += next
             
-            calculate(xor, start + 1)
-            calculate(next, start + 1)
+            calculate(xor, index + 1)
+            calculate(next, index + 1)
         }
         
         calculate(0, 0)

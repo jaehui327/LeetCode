ebaa808407e64b8e675985817e675bfc041bbb02
zhilogs <jaehui327@naver.com>
2022-04-14 13:07:45 +0900

    Time: 81 ms (80.00%), Space: 14.5 MB (33.33%) - LeetHub

--- 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.swift
+++ 1287-element-appearing-more-than-25-in-sorted-array/1287-element-appearing-more-than-25-in-sorted-array.swift
@@ -1,14 +1,11 @@
 class Solution {
     func findSpecialInteger(_ arr: [Int]) -> Int {
-        var dic: [Int: Int] = [:]
-        for a in arr {
-            if let _ = dic[a] {
-                dic[a]! += 1
-            } else {
-                dic[a] = 0
+        let standard = arr.count / 4
+        for i in 0..<arr.count - standard {
+            if arr[i] == arr[i + standard] {
+                return arr[i]
             }
         }
-        let result = dic.sorted { $0.value > $1.value }
-        return result.first!.key
+        return arr[-1]
     }
 }

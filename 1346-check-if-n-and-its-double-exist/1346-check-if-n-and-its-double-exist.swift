35d9c88968b85dc012b8b4f7360618db1e110b46
zhilogs <jaehui327@naver.com>
2022-05-05 22:00:40 +0900

    Time: 42 ms (14.18%), Space: 14.5 MB (14.18%) - LeetHub

--- 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.swift
+++ 1346-check-if-n-and-its-double-exist/1346-check-if-n-and-its-double-exist.swift
@@ -1,14 +1,11 @@
 class Solution {
     func checkIfExist(_ arr: [Int]) -> Bool {
-        for (i, a) in arr.enumerated() {
-            if a == 0 {
-                if i != arr.lastIndex(of: 0) {
-                    return true
-                }
-                continue
-            } else if arr.contains(a * 2) {
+        var seen = Set<Int>()
+        for a in arr {
+            if seen.contains(a * 2) || (a % 2 == 0 && seen.contains(a / 2)) {
                 return true
             }
+            seen.insert(a)
         }
         return false
     }
